493a7f6177979a00439c6415a1170e6df7bb6862
Daniel Alabuja <Danielal@HA-SHEM.com>
2019-06-17 13:27:21 +0100

    Books Api Endpoint.

--- LibraryCode/Controllers/BookController.cs
+++ LibraryCode/Controllers/BookController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LibraryCode.Controllers
 {
@@ -6,6 +10,64 @@ namespace LibraryCode.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly LibraryContext _context;
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookModel>>> GetAllBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookModel>> GetBook(long id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookModel>> PostBook(BookModel book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(book), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(long id, BookModel book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(long id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
